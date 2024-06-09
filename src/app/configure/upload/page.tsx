8ef2f3fb37346285fe6effb0c08e1b7ddba1d7cf
orzarhi <or.zarhi11@gmail.com>
2024-06-09 10:27:13 +0300

    feat: Add uploadthing module for file uploading functionality

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,24 +1,55 @@
 'use client'
 
 import { Progress } from "@/components/ui/progress"
+import { useToast } from "@/components/ui/use-toast"
+import { useUploadThing } from "@/lib/uploadthing"
 import { cn } from "@/lib/utils"
 import { Image, Loader2, MousePointerSquareDashed } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { useState, useTransition } from "react"
 import Dropzone, { FileRejection } from "react-dropzone"
 
 export default function page() {
+    const { toast } = useToast()
+
+    const router = useRouter()
+
     const [isDragOver, setIsDragOver] = useState<boolean>(false)
     const [uploadProgress, setUploadProgress] = useState<number>(0)
 
-    const onDropRejected = (files: FileRejection[]) => {
-        console.log(files)
+    const { startUpload, isUploading } = useUploadThing('imageUploader', {
+        onClientUploadComplete: ([data]) => {
+            const configId = data.serverData.configId
+            startTransition(() => {
+                router.push(`/configure/design?id=${configId}`)
+            })
+        },
+        onUploadProgress: (p) => {
+            setUploadProgress(p)
+        }
+    })
+
+
+    const onDropRejected = (rejectedFiles: FileRejection[]) => {
+        const [file] = rejectedFiles
+
+        setIsDragOver(false)
+
+        toast({
+            title: `${file.file.type} type is not supported.`,
+            description: 'Please choose a PNG, JPG or JPEG image instead.',
+            variant: 'destructive'
+
+        })
+
     }
 
-    const onDropAccepted = (files: File[]) => {
-        console.log(files)
+    const onDropAccepted = (acceptedFiles: File[]) => {
+        startUpload(acceptedFiles, { configId: undefined })
+
+        setIsDragOver(false)
     }
 
-    const isUploading = false
     const [isPending, startTransition] = useTransition()
 
     return (
@@ -64,7 +95,7 @@ export default function page() {
                                     </div>
                                 ) : isDragOver ? (
                                     <p>
-                                        <span className="font-semibold">Drop file</span>
+                                        <span className="font-semibold">Drop file</span>{' '}
                                         to upload
                                     </p>
                                 ) : (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
 import { Footer, Navbar } from "@/components";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,12 +22,15 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+
         <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">
             {children}
           </div>
           <Footer />
         </main>
+
+        <Toaster />
       </body>
     </html>
   );
