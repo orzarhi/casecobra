69d5a088cd4047e04946b54da7e2bf43621a0d2d
orzarhi <or.zarhi11@gmail.com>
2024-06-10 15:37:04 +0300

    feat: Add lazy loading for images and update dependencies

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,37 @@
+import { db } from '@/db'
+import { notFound } from 'next/navigation'
 import React from 'react'
+import { DesignConfigurator } from './DesignConfigurator'
 
-export default async function page() {
+
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined
+    }
+}
+
+export default async function page({ searchParams }: PageProps) {
+    const { id } = searchParams
+
+    if (!id || typeof id !== 'string') {
+        return notFound()
+    }
+
+    const configuration = await db.configuration.findUnique({
+        where: { id }
+    })
+
+    if (!configuration) {
+        return notFound()
+    }
+
+    const { imageUrl, width, height } = configuration
+
     return (
-        <div>page</div>
+        <DesignConfigurator
+            configId={configuration.id}
+            imageUrl={imageUrl}
+            imageDimensions={{ width, height }}
+        />
     )
 }
