32df75da07df742ca3d9bc76caf7162590d1cec6
orzarhi <or.zarhi11@gmail.com>
2024-06-11 10:37:41 +0300

    feat: Add formatPrice function for displaying price in DesignConfigurator

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -3,7 +3,7 @@
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import React, { useState } from 'react'
 import NextImage from 'next/image'
-import { cn } from '@/lib/utils'
+import { cn, formatPrice } from '@/lib/utils'
 import { Rnd } from 'react-rnd'
 import { HandleComponent } from '@/components/HandleComponent'
 import { ScrollArea } from '@/components/ui/scroll-area'
@@ -173,13 +173,17 @@ export const DesignConfigurator = ({ configId, imageUrl, imageDimensions }: Desi
                                                                 <RadioGroup.Label as='span' className='font-medium text-gray-900'>
                                                                     {option.label}
                                                                 </RadioGroup.Label>
-                                                                {option.description ?
+                                                                {option.description ? (
                                                                     <RadioGroup.Description as='span' className='text-gray-500'>
                                                                         <span className='block sm:inline'>{option.description}</span>
                                                                     </RadioGroup.Description>
-                                                                    : null}
+                                                                ) : null}
+
                                                             </span>
                                                         </span>
+                                                        <RadioGroup.Description as='span' className='mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right'>
+                                                            <span className='font-medium text-gray-900'>{formatPrice(option.price / 100)}</span>
+                                                        </RadioGroup.Description>
                                                     </RadioGroup.Option>
                                                 ))}
                                             </div>
