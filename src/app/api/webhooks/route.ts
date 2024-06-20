cb05a5f574db6a9a95c4eb0304f1f02e178b4d36
orzarhi <or.zarhi11@gmail.com>
2024-06-20 14:59:08 +0300

    feat: Add Stripe webhook route for handling checkout session completed event

---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,29 @@
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
+
+export async function POST(req: Request, res: Response) {
+    try {
+        const body = await req.text();
+        const signature = req.headers.get('stripe-signature');
+
+        if (!signature) {
+            return new Response('Invalid signature', { status: 400 });
+        }
+
+        const event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!);
+
+        if (event.type === 'checkout.session.completed') {
+            if (!event.data.object.customer_details?.email) {
+                throw new Error('Missing user email');
+            }
+            const session = event.data.object as Stripe.Checkout.Session;
+
+            const { userId, orderId } = session.metadata || {
+                userId: null,
+                orderId: null
+            }
+        }
+    } catch (error) {
+
+    }
+}
