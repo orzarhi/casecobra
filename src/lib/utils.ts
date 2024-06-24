a2657398bd5a5d00f6be2936d95dd514808015cb
orzarhi <or.zarhi11@gmail.com>
2024-06-24 16:59:47 +0300

    chore: Update metadata construction in layout.tsx and utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
@@ -11,4 +12,35 @@ export const formatPrice = (price: number) => {
     })
 
     return formatter.format(price)
+}
+
+export const constructMetadata = ({
+    title = 'CaseCobra - custom high-quality phone cases',
+    description = 'Create custom high-quality phone cases in seconds',
+    image = '/thumbnail.png',
+    icons = '/favicon.ico',
+}: {
+    title?: string
+    description?: string
+    image?: string
+    icons?: string
+} = {}): Metadata => {
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            images: [{ url: image }],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title,
+            description,
+            images: [image],
+            creator: '@orzarhi',
+        },
+        icons,
+        metadataBase: new URL('https://casecobra-orcode.vercel.app')
+    }
 }
